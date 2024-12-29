a88ad0a10490f81c06310e1acc93395a5ef2266b
Sergo <cekuritimafiz@gmail.com>
2024-12-29 20:13:29 +0300

    Update server.go
    making more files for better understanding

--- server.go
+++ server.go
@@ -1,71 +1,223 @@
-package main
+package auth
 
 import (
+	"encoding/json"
 	"fmt"
+	"html/template"
+	"test_proj/auth"
 	"log"
 	"net/http"
-	"os"
-
-	"github.com/go-redis/redis/v8"
 )
 
-// Config структура для хранения конфигурации
-type Config struct {
-	RedisAddr     string
-	RedisPassword string
-	SecretKey     string
-	EmailFrom     string
-	EmailPassword string
-	Port          string
+// Хранилище пользователей
+var users = map[string]string{
+	"student@example.com": "password123",
+	"teacher@example.com": "password456",
 }
 
-var config Config
+type AuthResponse struct {
+	Code string `json:"code"`
+}
+type CodeResponse struct {
+	Token   string `json:"token"`
+	Success bool   `json:"success"`
+	Message string `json:"message"`
+}
 
-func loadConfig() {
-	config = Config{
-		RedisAddr:     getEnv("REDIS_ADDR", "localhost:6379"),
-		RedisPassword: getEnv("REDIS_PASSWORD", ""),
-		SecretKey:     getEnv("SECRET_KEY", "your_secret_key"),
-		EmailFrom:     getEnv("EMAIL_FROM", "example@gmail.com"),
-		EmailPassword: getEnv("EMAIL_PASSWORD", "password"),
-		Port:          getEnv("PORT", "8080"),
+func authenticate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		renderTemplate(w, "login.html", nil)
+		return
+	}
+
+	email := r.FormValue("email")
+	password := r.FormValue("password")
+
+	if storedPassword, exists := users[email]; exists && storedPassword == password {
+		user := auth.User{
+			Email: email,
+			Roles: []string{"Студент"}, // Можно добавить логику для установки ролей
+		}
+
+		authConfig := auth.NewAuthConfig()
+		token, err := authConfig.CreateAccessToken(user)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Создание кука с токеном
+		cookie := http.Cookie{
+			Name:     "access_token",
+			Value:    token,
+			Path:     "/",
+			MaxAge:   300, // Срок действия кука в секундах (5 минут)
+			HttpOnly: true,
+		}
+		http.SetCookie(w, &cookie)
+
+		// Отправка ответа
+		renderTemplate(w, "protected.html", map[string]string{
+			"Email": email,
+			"Token": token,
+		})
+	} else {
+		http.Error(w, `{"error": "Invalid credentials"}`, http.StatusUnauthorized)
 	}
 }
 
-func getEnv(key string, defaultValue string) string {
-	value := os.Getenv(key)
-	if value == "" {
-		return defaultValue
+func requestCodeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Неправильный метод запроса", http.StatusMethodNotAllowed)
+		return
 	}
-	return value
+	token := r.URL.Query().Get("token")
+	if token == "" {
+		http.Error(w, "token must be provided", http.StatusBadRequest)
+		return
+	}
+	// Проверяем, есть ли email
+	email := r.URL.Query().Get("email")
+	if email == "" {
+		// Если email не указан, рендерим форму
+		renderTemplate(w, "request_code.html", nil)
+		return
+	}
+
+	// Генерация и отправка кода
+	authConfig := auth.NewAuthConfig()
+	code, err := authConfig.HandleAuthCodeRequest(token)
+
+	if err != nil {
+		http.Error(w, "Ошибка при отправке email", http.StatusInternalServerError)
+		return
+	}
+
+	jsonResponse, _ := json.Marshal(AuthResponse{Code: code})
+
+	// Рендерим подтверждение
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonResponse)
 }
+func codeAuthentication(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Неправильный метод запроса", http.StatusMethodNotAllowed)
+		return
+	}
+	// Проверяем, есть ли code
+	code := r.URL.Query().Get("code")
+	if code == "" {
+		http.Error(w, "Code must be provided", http.StatusBadRequest)
+		return
+	}
+	refreshToken := r.URL.Query().Get("refreshToken")
+	if refreshToken == "" {
+		http.Error(w, "refresh token must be provided", http.StatusBadRequest)
+		return
+	}
+	authConfig := auth.NewAuthConfig()
+	token, ok, err := authConfig.HandleCodeAuthentication(code, refreshToken)
 
-var Rdb *redis.Client
+	var jsonResponse []byte
+	if !ok {
+		message := "token is not valid"
+		if err != nil {
+			message = err.Error()
+		}
+		jsonResponse, _ = json.Marshal(CodeResponse{
+			Token:   "",
+			Success: false,
+			Message: message,
+		})
 
-func initRedis() {
-	Rdb = redis.NewClient(&redis.Options{
-		Addr:     config.RedisAddr,
-		Password: config.RedisPassword,
-		DB:       0,
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(jsonResponse)
+		return
+	}
+
+	jsonResponse, _ = json.Marshal(CodeResponse{
+		Token:   token,
+		Success: true,
+		Message: "success",
+	})
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonResponse)
+}
+func authStatus(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Неправильный метод запроса", http.StatusMethodNotAllowed)
+		return
+	}
+	token := r.URL.Query().Get("token")
+	if token == "" {
+		http.Error(w, "token must be provided", http.StatusBadRequest)
+		return
+	}
+	authConfig := auth.NewAuthConfig()
+	ok, err := authConfig.validateAuthInfo(token)
+	if err != nil {
+		http.Error(w, "token is not valid", http.StatusBadRequest)
+		return
+	}
+	if !ok {
+		http.Error(w, "token is expired", http.StatusBadRequest)
+		return
+	}
+	authConfig.updateAuthInfo(token, "received")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("token status updated"))
+}
+func handleSuccessLogin(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	token := r.FormValue("token")
+	if token == "" {
+		http.Error(w, "token must be provided", http.StatusBadRequest)
+		return
+	}
+	authConfig := auth.NewAuthConfig()
+	_, err := authConfig.validateAuthInfo(token)
+	if err != nil {
+		http.Error(w, "token not valid", http.StatusBadRequest)
+		return
+	}
+	renderTemplate(w, "protected.html", map[string]string{
+		"Token": token,
 	})
 }
 
+// Функция для рендеринга шаблонов
+func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
+	t, err := template.ParseFiles("templates/" + tmpl)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	t.Execute(w, data)
+}
+func index(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "index.html", nil)
+}
 func main() {
-	loadConfig() // Загрузка конфигурации
-	initRedis()  // Инициализация Redis
-
-	// Регистрация маршрутов (пусть функции-обработчики определены в handlers.go)
+	// Регистрация маршрутов
 	http.HandleFunc("/", index)
 	http.HandleFunc("/login", authenticate)
-	http.HandleFunc("/logout", logout)
-	http.HandleFunc("/protected", protected)
-	http.HandleFunc("/request-code", requestCodeHandler)
-	http.HandleFunc("/auth/yandex", yandexLoginHandler)
-	http.HandleFunc("/auth/github", githubLoginHandler)
+	http.HandleFunc("/auth-code-request", requestCodeHandler)
+	http.HandleFunc("/code-authentication", codeAuthentication)
+	http.HandleFunc("/auth-status", authStatus)
+	http.HandleFunc("/success-login", handleSuccessLogin)
 
-	fmt.Printf("Server is running on port %s\n", config.Port)
-	err := http.ListenAndServe(":"+config.Port, nil)
+	fmt.Println("Go server is running on port 8081")
+	err := http.ListenAndServe(":8081", nil) // Запускаем сервер на порту 8081
 	if err != nil {
-		log.Fatal("Failed to start server: ", err)
+		log.Fatal("Failed to start Go server", err)
 	}
 }
